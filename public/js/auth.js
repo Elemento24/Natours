a163bba834d6572155a1001168387aab064681c5
Elemento24 <mittalvishesh021@gmail.com>
2020-11-15 12:05:54 +0530

    Rename login.js to auth.js and add the Signup Function in it

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,6 +2,31 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
+export const signup = async (name, email, password, passwordConfirm, btn) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Signed Up successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+    btn.textContent = 'Signup';
+  }
+};
+
 export const login = async (email, password) => {
   try {
     const res = await axios({
